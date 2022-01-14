7d7ff0582d53fecc7b782ed479e5102dc7a5b06a
Shazwi <supershazwi@gmail.com>
2022-01-15 01:08:19 +0800

    completed count style rule usage

--- parseJs.js
+++ parseJs.js
@@ -2,6 +2,7 @@ import { readFile } from "fs";
 import { rgbToHex } from "./cssConvert.js";
 
 const colorObject = {};
+const styleObject = {};
 
 const showFileColors = (error, content) => {
   if (error) {
@@ -15,6 +16,8 @@ const showFileColors = (error, content) => {
     let sentence = split[i];
 
     if (sentence.indexOf("#") !== -1) {
+      let style = sentence.slice(0, sentence.indexOf(":"));
+
       // this is a function
       const hashIndex = sentence.indexOf("#");
       let color = sentence.slice(hashIndex, sentence.indexOf(";"));
@@ -31,36 +34,60 @@ const showFileColors = (error, content) => {
         color = color.slice(0, commaIndex);
       }
 
-      if (color.length !== 1) {
-        if (colorObject[color] === undefined) {
-          colorObject[color] = 1;
-        } else {
-          colorObject[color] += 1;
-        }
-      }
+      addToColorObject(color);
+      addToStyleObject(style.trim());
     }
 
     if (sentence.indexOf("rgb(") !== -1) {
+      let style = sentence.slice(0, sentence.indexOf(":"));
+
       sentence = sentence.slice(sentence.indexOf("rgb("));
       sentence = sentence.slice(0, sentence.indexOf(")"));
       sentence += ")";
 
       const color = rgbToHex(sentence);
 
-      if (color.length !== 1) {
-        if (colorObject[color] === undefined) {
-          colorObject[color] = 1;
-        } else {
-          colorObject[color] += 1;
-        }
-      }
+      addToColorObject(color);
+      addToStyleObject(style.trim());
     }
   }
+
+  console.log('-------');
+  console.log('colours:');
+  console.log('-------');
 
   for (const color in colorObject) {
     console.log(`${color}: ${colorObject[color]}`);
   }
+
+  console.log('-------');
+  console.log('styles:');
+  console.log('-------');
+
+  for (const style in styleObject) {
+    console.log(`${style}: ${styleObject[style]}`);
+  }
 };
+
+const addToColorObject = (color) => {
+  if (color.length !== 1) {
+    if (colorObject[color] === undefined) {
+      colorObject[color] = 1;
+    } else {
+      colorObject[color] += 1;
+    }
+  }
+}
+
+const addToStyleObject = (style) => {
+  if (style.length !== 1) {
+    if (styleObject[style] === undefined) {
+      styleObject[style] = 1;
+    } else {
+      styleObject[style] += 1;
+    }
+  }
+}
 
 export const showColors = (fileName) => {
   readFile(fileName, "utf8", showFileColors);
